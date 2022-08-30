0790043f8d30cbaef3675319ce1e9a546fc3c277
Amanuel Abay <amanab.449@gmail.com>
2022-08-30 16:46:17 +0300

    final change

--- adployee.js
+++ adployee.js
@@ -9,7 +9,7 @@ import mongoose from "mongoose";
 const adployee = express();
 dotenv.config();
 
-const url = process.env.mongodb_url || "mongodb://0.0.0.0:27017/employee?";
+
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
-export const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"].split(" ")[1];
+export const verifyToken = async (req, res, next) => {
+  const token = await req.headers["authorization"].split(" ")[1];
   if (!token) {
     return next(createError(401, "You are not authenticated!"));
   }
